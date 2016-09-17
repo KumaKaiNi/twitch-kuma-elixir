b8a04c3fdc963e9e31aab5eca4bdf3018a916069
rekyuu <senkan.re.kyuu@gmail.com>
2016-09-16 20:56:00 -0600

    Update twitch_kuma.ex

--- lib/twitch_kuma.ex
+++ lib/twitch_kuma.ex
@@ -1,2 +1,13 @@
 defmodule TwitchKuma do
+  use Kaguya.Module, "main"
+  
+  handle "PRIVMSG" do
+    match ["!ping", "!p"], :pingHandler
+    match "hi", :hiHandler
+    match "!say ~message", :sayHandler
+  end
+
+  defh pingHandler, do: reply "pong!"
+  defh hiHandler(%{user: %{nick: nick}}), do: reply "hi #{nick}!"
+  defh sayHandler(%{"message" => response}), do: reply response
 end
