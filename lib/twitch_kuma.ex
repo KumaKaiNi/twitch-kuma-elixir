bcab502c73b5d639ba8265e70e7a6011ae40bef6
rekyuu <senkan.re.kyuu@gmail.com>
2016-09-16 21:26:16 -0600

    Update twitch_kuma.ex

--- lib/twitch_kuma.ex
+++ lib/twitch_kuma.ex
@@ -1,10 +1,28 @@
 defmodule TwitchKuma do
   use Kaguya.Module, "main"
   
+  validator :is_rekyuu do
+    :rekyuu_check
+  end
+  
+  def rekyuu_check(%{user: %{nick: nick}, args: [chan]}) do
+    pid = Kaguya.Util.getChanPid(chan)
+    user = GenServer.call(pid, {:get_user, nick})
+
+    if user == nil do
+      false
+    else
+      user.nick == "rekyuu_senkan"
+    end
+  end
+  
   handle "PRIVMSG" do
-    match ["!ping", "!p"], :pingHandler
     match "hi", :hiHandler
     match "!say ~message", :sayHandler
+    
+    validate :is_rekyuu do
+      match ["!ping", "!p"], :pingHandler
+    end
   end
 
   defh pingHandler, do: reply "pong!"
