c80247f8f86d4cbaa1f3f605fed09a3a608a2006
rekyuu <senkan.re.kyuu@gmail.com>
2016-09-18 02:09:56 -0600

    changed dets files

--- lib/twitch_kuma/util.ex
+++ lib/twitch_kuma/util.ex
@@ -5,13 +5,13 @@ defmodule TwitchKuma.Util do
   def percent(n), do: Enum.random(1..100) <= n
 
   def store_data(table, key, value) do
-    {:ok, db} = :dets.open_file(table, [type: :set])
+    {:ok, db} = :dets.open_file(table, [type: :set, file: app_dir <> "#{table}.dets"])
     :dets.insert(db, {key, value})
     :dets.close(db)
   end
 
   def query_data(table, key) do
-    {:ok, db} = :dets.open_file(table, [type: :set])
+    {:ok, db} = :dets.open_file(table, [type: :set, file: app_dir <> "#{table}.dets"])
     result = :dets.lookup(db, key)
 
     response =
@@ -25,7 +25,7 @@ defmodule TwitchKuma.Util do
   end
 
   def delete_data(table, key) do
-    {:ok, db} = :dets.open_file(table, [type: :set])
+    {:ok, db} = :dets.open_file(table, [type: :set, file: app_dir <> "#{table}.dets"])
     response = :dets.delete(db, key)
 
     :dets.close(db)
