fc1914c7ccd58b753ac31ce03aa9f919bf9f3867
rekyuu <senkan.re.kyuu@gmail.com>
2017-08-27 14:00:52 -0600

    edited so you dont HAVE to level to post links

--- lib/twitch_kuma.ex
+++ lib/twitch_kuma.ex
@@ -177,34 +177,37 @@ defmodule TwitchKuma do
   defh moderate do
     words = message.trailing |> String.split
     stats = query_data(:stats, message.user.nick)
+    coins = query_data(:bank, message.user.nick)
 
     unless stats do
-      links = for word <- words do
-        uri = case URI.parse(word) do
-          %URI{host: nil, path: path} ->
-            if length((path |> String.split(".")) -- [""]) >= 2 do
-              path = "www." <> path
-              Logger.warn "Banning for posting link: #{path}"
-              :inet.gethostbyname(String.to_charlist(path))
-            else
-              nil
-            end
-          %URI{host: host} ->
-            Logger.warn "Banning for posting link: #{host}"
-            :inet.gethostbyname(String.to_charlist(host))
-          uri -> nil
+      unless coins >= 256 do
+        links = for word <- words do
+          uri = case URI.parse(word) do
+            %URI{host: nil, path: path} ->
+              if length((path |> String.split(".")) -- [""]) >= 2 do
+                path = "www." <> path
+                Logger.warn "Banning for posting link: #{path}"
+                :inet.gethostbyname(String.to_charlist(path))
+              else
+                nil
+              end
+            %URI{host: host} ->
+              Logger.warn "Banning for posting link: #{host}"
+              :inet.gethostbyname(String.to_charlist(host))
+            uri -> nil
+          end
+
+          case uri do
+            {:ok, _} -> true
+            {:error, _} -> false
+            nil -> false
+          end
         end
 
-        case uri do
-          {:ok, _} -> true
-          {:error, _} -> false
-          nil -> false
-        end
-      end
-
-      if links do
-        if Enum.member?(links, true) do
-          reply "/timeout #{message.user.nick} 1 You must be Level 2 to post links."
+        if links do
+          if Enum.member?(links, true) do
+            reply "/timeout #{message.user.nick} 1 You must be Level 2 to post links."
+          end
         end
       end
     end
