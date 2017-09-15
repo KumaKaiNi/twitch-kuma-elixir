da098750516620d80369cdaa4ad1ed56b92172c7
rekyuu <senkan.re.kyuu@gmail.com>
2017-09-14 18:55:23 -0600

    fixed message model

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule TwitchKuma.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :kaguya]]
+    [applications: [:logger, :kaguya, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +28,7 @@ defmodule TwitchKuma.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:kaguya, "~> 0.5.0"},
-     {:poison, "~> 3.1"}]
+     {:poison, "~> 3.1"},
+     {:httpoison, "~> 0.13"}]
   end
 end
